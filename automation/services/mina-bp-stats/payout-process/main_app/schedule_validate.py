a0eee43b9b8ced10d920296986421a233b5dc36b
Supriya Bhapkar <umesh@ontab.com>
2022-04-15 11:37:18 +0000

    scheduler validate  script added by supriya

---
+++ automation/services/mina-bp-stats/payout-process/main_app/schedule_validate.py
@@ -0,0 +1,77 @@
+import subprocess
+import datefinder
+from datetime import datetime, timedelta
+import time
+from apscheduler.schedulers.blocking import BlockingScheduler
+import pytz
+from dateutil.relativedelta import relativedelta
+
+utc = pytz.UTC
+
+
+def find_epoch_no():
+    print("Starting Of python Script")
+    script_output = subprocess.call(['python', r"/home/supriyab/PycharmProjects/minanet_phase_2/payouts_validate.py"])
+    return script_output
+
+
+def extract_date_from_text_file():
+    file = open("genesis.txt", 'r')
+    content = file.read()
+    matches = list(datefinder.find_dates(content))
+    if len(matches) > 0:
+        for genesis_t in matches:
+            print(genesis_t)
+    else:
+        print("Found no dates.")
+    return genesis_t
+
+
+def find_out_current_date():
+    current_date = datetime.today()
+    return current_date
+
+
+def find_next_job_time(genesis_t, minutes_to_add):
+    next_job_time = genesis_t + timedelta(minutes=minutes_to_add)
+    next_job_time = next_job_time.replace(tzinfo=None)
+    return next_job_time
+
+
+def main():
+    script_output = find_epoch_no()
+    if script_output <= 1:
+        minutes_to_add = 10
+
+    else:
+        minute_per_epoch = 21420
+        next_epoch_number = script_output + 2
+        minutes_to_add = minute_per_epoch * next_epoch_number
+        minutes_to_add = (minutes_to_add + (3500 * 3))
+
+    genesis_t = extract_date_from_text_file()
+    next_job_time = find_next_job_time(genesis_t, minutes_to_add)
+
+    current_date = find_out_current_date()
+
+    if current_date > next_job_time:
+        str_minutes = 10
+        next_job_time = current_date + timedelta(minutes=str_minutes)
+
+    next_day = next_job_time + timedelta(days=1)
+    next_day = next_day.date()
+    next_day = next_day + relativedelta(minutes=30)
+
+    # Scheduling task
+    def job(text):
+        t = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        print('{} --- {}'.format(text, t))
+
+    scheduler = BlockingScheduler()
+
+    scheduler.add_job(job, 'interval', minutes=0, seconds=0, start_date=next_day, args=['job2'])
+    scheduler.start()
+
+
+if __name__ == '__main__':
+    main()
