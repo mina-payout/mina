c6a81ca61cc4d9bd6e39bd4ccc54f72cab8fd1a5
umesh-ontab <81137895+mina-payout@users.noreply.github.com>
2022-04-16 16:20:38 +0530

    updated code

--- automation/services/mina-bp-stats/leaderboard-api/minanet_app/flask_api.py
+++ automation/services/mina-bp-stats/leaderboard-api/minanet_app/flask_api.py
@@ -36,21 +36,56 @@ def get_json_data_current(conn=connection_snark):
     query = """SELECT block_producer_key , score ,score_percent FROM nodes WHERE application_status = true and score 
     is not null ORDER BY score DESC """
     try:
-        cursor = connection_sd.cursor()
+        cursor = conn.cursor()
         cursor.execute(query)
         result = [dict((cursor.description[i][0], str(value)) for i, value in enumerate(row)) for row in cursor.fetchall()]
-        connection_sd.commit()
         logger.info("fetch data for flask app...")
     except (Exception, psycopg2.DatabaseError) as error:
         logger.info(ERROR.format(error))
-        connection_sd.rollback()
         cursor.close()
         return -1
     finally:
-        connection_sd.commit()
         cursor.close()
     return result
 
+#  get score at specific time
+def get_json_data(conn=connection_snark, score_at=None):
+    if 'current'==score_at:
+        return get_json_data_current(conn)
+
+    query = """with vars  (snapshot_date, start_date) as( values ('%s'::timestamp , 
+			('%s'::timestamp)- interval '60' day )
+	)
+	, epochs as(
+			select extract('epoch' from snapshot_date) as end_epoch, 
+		extract('epoch' from start_date) as start_epoch from vars
+	)
+	,  b_logs as(
+		select (count(1) ) as surveys 
+		from bot_logs b , epochs e
+		where b.batch_start_epoch between start_epoch and end_epoch 
+	)
+	,lastboard as (
+         select node_id,count(distinct bot_log_id) total_points
+         from  points prt , vars 
+         where (file_timestamps + (330 * interval '1 minute')) between start_date and snapshot_date
+         group by node_id
+     )
+         select node_id, n.block_producer_key , total_points, surveys,  trunc( ((total_points::decimal*100) / surveys),2) as score_perc
+         from lastboard l join nodes n on l.node_id=n.id, b_logs t 
+         order by 3 desc """
+    try:
+        cursor = conn.cursor()
+        cursor.execute(query, (score_at, ))
+        result = [dict((cursor.description[i][0], str(value)) for i, value in enumerate(row)) for row in cursor.fetchall()]
+        logger.info("fetch data for flask app...")
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.info(ERROR.format(error))
+        cursor.close()
+        return -1
+    finally:
+        cursor.close()
+    return result
 
 # create app instance
 app = Flask(__name__)
@@ -64,13 +99,14 @@ swagger = Swagger(app)
 @swag_from('api_spec_without_data_type_score_at.yml', endpoint='without_data_type_score_at')
 @swag_from('api_spec_without_score_at.yml', endpoint='without_score_at')
 @swag_from('api_spec.yml', endpoint='all')
-def get_score(dataType, scoreAt):
+def get_score(dataType='snarkwork', scoreAt='current'):
     data = None
     logger.info('dt: {0}, time:{1}'.format(dataType, scoreAt))
     if 'snarkwork' == dataType:
-        data = get_json_data_current(connection_snark)
+        data = get_json_data(connection_snark, scoreAt)
     elif 'sidecar' == dataType:
-        data = get_json_data_current(connection_sd)
+        data = get_json_data(connection_sd, scoreAt)
+    
     return jsonify(data)
 
 
