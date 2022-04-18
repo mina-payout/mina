6ee5b8057195d1312067bb0c20e4655e02492264
umesh-ontab <81137895+mina-payout@users.noreply.github.com>
2022-04-18 10:35:52 +0530

    removed maintenance mode

--- automation/services/mina-bp-stats/leaderboard-ui/web-dev/php/config.php
+++ automation/services/mina-bp-stats/leaderboard-ui/web-dev/php/config.php
@@ -2,7 +2,7 @@
 
 $ShowScoreColumn = true;
 $Configurl = "https://docs.google.com/spreadsheets/d/1JTp7nZiw68a-m5KoBeqPr0OOWBVO804xv6HpQNQdKPg/edit#gid=0";
-$MaintenanceMode = true;
+$MaintenanceMode = false;
 return array($ShowScoreColumn,$Configurl,$MaintenanceMode);
 
 ?>
