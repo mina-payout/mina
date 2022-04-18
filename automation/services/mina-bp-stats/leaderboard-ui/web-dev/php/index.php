b760fd0c8a812e7b5d7de8beedddcd701a87dedb
umesh-ontab <81137895+mina-payout@users.noreply.github.com>
2022-04-18 18:25:01 +0530

    logo issue

--- automation/services/mina-bp-stats/leaderboard-ui/web-dev/php/index.php
+++ automation/services/mina-bp-stats/leaderboard-ui/web-dev/php/index.php
@@ -32,7 +32,7 @@
 
         <!-- Logo And Header Section Start -->
         <div class="row mb-3 minalogo">
-               <img src="assets/images/MinaWordmark.png" alt="Mina" class="mina-main-logo">
+               <img src="assets/images/mina-wordmark-light.svg" alt="Mina" class="mina-main-logo">
         </div>
         <div class="row mb-5">
             <div class="subheader">
