81da27ed259ecfd7e0f7bf1601eba8eb72ebcc8b
Mehul Wankhede <mehul.wankhede@bnt.local>
2023-01-24 17:03:06 +0530

    new changes added

--- automation/services/mina-bp-stats/payout-process/main_app/calculate_email.py
+++ automation/services/mina-bp-stats/payout-process/main_app/calculate_email.py
@@ -7,7 +7,6 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail, Attachment, FileContent, FileName, FileType, Disposition
 from logger_util import logger
 
-
 connection_leaderboard = psycopg2.connect(
     host=BaseConfig.POSTGRES_LEADERBOARD_HOST,
     port=BaseConfig.POSTGRES_LEADERBOARD_PORT,
@@ -18,7 +17,7 @@ connection_leaderboard = psycopg2.connect(
 
 
 def get_block_producer_mail(winner_bpk):
-    mail_id_sql = """select block_producer_email from node_record_table where block_producer_key = %s"""
+    mail_id_sql = """select block_producer_email from nodes where block_producer_key = %s"""
     cursor = connection_leaderboard.cursor()
     email = ''
     try:
@@ -29,16 +28,17 @@ def get_block_producer_mail(winner_bpk):
             if email == '':
                 logger.warning("email not found for :".format(winner_bpk))
             if BaseConfig.OVERRIDE_EMAIL:
-                    email = BaseConfig.OVERRIDE_EMAIL
+                email = BaseConfig.OVERRIDE_EMAIL
     except (Exception, psycopg2.DatabaseError) as error:
         logger.info("Error: {0} ".format(error))
         cursor.close()
     return email
 
+
 def send_mail(epoch_id, delegate_record_df):
     # read the data from delegation_record_table
     payouts_df = delegate_record_df
-    total_minutes = (int(epoch_id+1) * 7140 * 3) + (BaseConfig.SLOT_WINDOW_VALUE * 3)
+    total_minutes = (int(epoch_id + 1) * 7140 * 3) + (BaseConfig.SLOT_WINDOW_VALUE * 3)
     deadline_date = BaseConfig.GENESIS_DATE + datetime.timedelta(minutes=total_minutes)
     current_date = datetime.datetime.now()
     day_count = deadline_date - current_date
@@ -48,7 +48,7 @@ def send_mail(epoch_id, delegate_record_df):
     # reading email template
     f = open(BaseConfig.CALCULATE_EMAIL_TEMPLATE, "r")
     html_text = f.read()
-    
+
     count = 0
     for i in range(payouts_df.shape[0]):
         count = count + 1
@@ -59,8 +59,8 @@ def send_mail(epoch_id, delegate_record_df):
         # Adding dynamic values into the template
         html_content = html_content.replace("#FOUNDATION_ADDRESS", str(payouts_df.iloc[i, 0]))
         html_content = html_content.replace("#PAYOUT_AMOUNT", str(payouts_df.iloc[i, 3]))
-        html_content = html_content.replace("#EPOCH_NO", str(epoch_id)) 
-        html_content = html_content.replace("#CURRENT_EPOCH_NO", str(epoch_id+1))
+        html_content = html_content.replace("#EPOCH_NO", str(epoch_id))
+        html_content = html_content.replace("#CURRENT_EPOCH_NO", str(epoch_id + 1))
         html_content = html_content.replace("#BLOCK_PRODUCER_ADDRESS", str(payouts_df.iloc[i, 1]))
         html_content = html_content.replace("#DEADLINE_DATE", str(deadline_date))
         html_content = html_content.replace("#DAY_COUNT", str(day_count))
@@ -75,8 +75,13 @@ def send_mail(epoch_id, delegate_record_df):
         try:
             sg = SendGridAPIClient(api_key=BaseConfig.SENDGRID_API_KEY)
             response = sg.send(message)
-            logger.info('email sent to: delgate:{0}, delgatee: {1}, emailid: {2}, status {3}, messageId: {4}'.format(delegate, delegatee, 
-                block_producer_email, response.status_code, response.headers.get_all('X-Message-Id')))           
+            logger.info(
+                'email sent to: delgate:{0}, delgatee: {1}, emailid: {2}, status {3}, messageId: {4}'.format(delegate,
+                                                                                                             delegatee,
+                                                                                                             block_producer_email,
+                                                                                                             response.status_code,
+                                                                                                             response.headers.get_all(
+                                                                                                                 'X-Message-Id')))
         except Exception as e:
             logger.error('Error sending email: delgate:{0}, delgatee: {1}, error: {2}'.format(delegate, delegatee, e))
-    logger.info("Calculation: epoch number: {0}, emails sent: {1}".format(epoch_id,count))
+    logger.info("Calculation: epoch number: {0}, emails sent: {1}".format(epoch_id, count))
