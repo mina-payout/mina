8d1d69191938b7949a518d3d475497dc703b86fd
Mehul Wankhede <mehul.wankhede@bnt.local>
2023-01-24 17:27:36 +0530

    new changes added

--- automation/services/mina-bp-stats/payout-process/main_app/payouts_calculate.py
+++ automation/services/mina-bp-stats/payout-process/main_app/payouts_calculate.py
@@ -39,7 +39,6 @@ def get_gcs_client():
     return storage.Client()
 
 
-
 def read_staking_json_list():
     storage_client = get_gcs_client()
     bucket = storage_client.get_bucket(BaseConfig.GCS_BUCKET_NAME)
@@ -74,13 +73,15 @@ def get_last_processed_epoch_from_audit():
         cursor.close()
     return last_epoch
 
+
 # determine whether process can run now for given epoch number
 def can_run_job(next_epoch):
-    next_epoch_end = (int(next_epoch+1) * 7140 * 3) + (100 * 3)
+    next_epoch_end = (int(next_epoch) * 7140 * 3) + (100 * 3)
     next_job_time = BaseConfig.GENESIS_DATE + timedelta(minutes=next_epoch_end)
     next_job_time = next_job_time.replace(tzinfo=timezone.utc)
     next_job_time = next_job_time + timedelta(days=1)
-    next_job_time= next_job_time.replace(hour=00, minute=30)
+    next_job_time = next_job_time.replace(hour=00, minute=30)
+    logger.info("can_run_job -> Next job time: {0}".format(next_job_time))
     current_time = datetime.now(timezone.utc)
     if next_job_time > current_time:
         result = False
@@ -88,24 +89,26 @@ def can_run_job(next_epoch):
         result = True
     return result
 
+
 # this will check audit log table, and will determine last processed epoch
 # if no entries found, default to first epoch
 def initialize():
     result = 0
     last_epoch = get_last_processed_epoch_from_audit()
-    if can_run_job(last_epoch+1) :
-        result = main(last_epoch + 1, True)
+    if can_run_job(last_epoch + 1):
+        result = main(last_epoch + 1, BaseConfig.SEND_EMAIL_TO_BP)
     else:
-        result = last_epoch 
+        result = last_epoch
     return result
 
 
-# for epoch 0 & epoch 1 
+# for epoch 0 & epoch 1
 #   - have to use same staking ledger 'staking-1'
 #   - blocks produced would be for epoch 0 & epoch 1
 #   - payment recieved would be for epoch 0 & epoch 1
 def is_genesis_epoch(epoch_id):
-    return True if int(epoch_id) <2 else False
+    return True if int(epoch_id) < 2 else False
+
 
 def read_staking_json_for_epoch(epoch_id):
     storage_client = get_gcs_client()
@@ -113,7 +116,7 @@ def read_staking_json_for_epoch(epoch_id):
     if is_genesis_epoch(epoch_id):
         staking_file_prefix = "staking-1-"
     else:
-        staking_file_prefix = "staking-" + str(epoch_id)
+        staking_file_prefix = "staking-" + str(epoch_id) + "-"
     blobs = storage_client.list_blobs(bucket, prefix=staking_file_prefix)
     # convert to string
     ledger_name = ''
@@ -160,11 +163,41 @@ def insert_data(df, page_size=100):
         cursor.close()
     return result
 
+
 def truncate(number, digits=5) -> float:
     stepper = 10.0 ** digits
     return math.trunc(stepper * number) / stepper
 
-def calculate_payout(delegation_record_list, modified_staking_df, foundation_bpk, epoch_id):
+
+def get_blocks_produced_for_all(epoch_no):
+    # calculate blocks produced by delegate
+    query = '''WITH RECURSIVE chain AS (
+    (SELECT b.id, b.state_hash,parent_id, b.creator_id,b.height,b.global_slot_since_genesis/7140 AS epoch,b.staking_epoch_data_id FROM blocks b WHERE height = (select MAX(height) from blocks)
+    ORDER BY timestamp ASC
+    LIMIT 1)
+    UNION ALL
+    SELECT b.id, b.state_hash,b.parent_id, b.creator_id,b.height,b.global_slot_since_genesis/7140 AS epoch,b.staking_epoch_data_id FROM blocks b
+    INNER JOIN chain
+    ON b.id = chain.parent_id AND chain.id <> chain.parent_id
+    ) SELECT count(distinct c.id) as blocks_produced, pk.value as creator
+    FROM chain c INNER JOIN blocks_internal_commands bic  on c.id = bic.block_id
+    INNER JOIN public_keys pk ON pk.id = c.creator_id
+    WHERE epoch = %s
+    GROUP BY pk.value;
+    '''
+    cursor = connection_archive.cursor()
+    try:
+        cursor.execute(query, (epoch_no,))
+        blocks_produced_list = cursor.fetchall()
+        df = pd.DataFrame(blocks_produced_list,
+                          columns=['blocks_produced', 'creator'])
+    except (Exception, psycopg2.DatabaseError) as error:
+        logger.error(ERROR.format(error))
+        cursor.close()
+    return df
+
+
+def calculate_payout(delegation_record_list, modified_staking_df, foundation_bpk, epoch_id, blocks_produced_df):
     filter_stake_df = modified_staking_df[modified_staking_df['pk'] == foundation_bpk]
     # calculate provider delegates accounts
     delegate_bpk = filter_stake_df['delegate'].values[0]
@@ -187,34 +220,14 @@ def calculate_payout(delegation_record_list, modified_staking_df, foundation_bpk
     payout = (provider_share * 0.95) * BaseConfig.COINBASE
 
     # calculate blocks produced by delegate
-    query = '''WITH RECURSIVE chain AS (
-    (SELECT b.id, b.state_hash,parent_id, b.creator_id,b.height,b.global_slot_since_genesis/7140 AS epoch,b.staking_epoch_data_id FROM blocks b WHERE height = (select MAX(height) from blocks)
-    ORDER BY timestamp ASC
-    LIMIT 1)
-    UNION ALL
-    SELECT b.id, b.state_hash,b.parent_id, b.creator_id,b.height,b.global_slot_since_genesis/7140 AS epoch,b.staking_epoch_data_id FROM blocks b
-    INNER JOIN chain
-    ON b.id = chain.parent_id AND chain.id <> chain.parent_id
-    ) SELECT count(distinct c.id) as blocks_produced, pk.value as creator
-    FROM chain c INNER JOIN blocks_internal_commands bic  on c.id = bic.block_id
-    INNER JOIN public_keys pk ON pk.id = c.creator_id
-    WHERE pk.value= %s and epoch = %s
-    GROUP BY pk.value;
-    '''
-    cursor = connection_archive.cursor()
-    try:
-        cursor.execute(query, (delegate_bpk, epoch_id))
-        blocks_produced_list = cursor.fetchall()
-    except (Exception, psycopg2.DatabaseError) as error:
-        logger.error(ERROR.format(error))
-        cursor.close()
-
-    blocks_produced = 0
-    if blocks_produced_list is not None and len(blocks_produced_list) > 0:
-        blocks_produced = blocks_produced_list[0][0]
-    delegation_record_dict['blocks'] = blocks_produced
+    current_acc_df = blocks_produced_df.query('creator==@delegate_bpk')
+    if not current_acc_df.empty:
+        blocks_produced = current_acc_df.iloc[0]['blocks_produced']
+    else:
+        blocks_produced = 0
 
     # calculate total payout
+    delegation_record_dict['blocks'] = blocks_produced
     total_payout = payout * blocks_produced
     total_payout = truncate(total_payout, 5)
     delegation_record_dict['payout_amount'] = total_payout
@@ -241,8 +254,9 @@ def insert_into_audit_table(file_name, epoch_id):
         cursor.close()
         connection_payout.commit()
 
+
 def insert_into_staking_ledger(modified_staking_df, epoch_no):
-    modified_staking_df['epoch_number']= epoch_no
+    modified_staking_df['epoch_number'] = epoch_no
     tuples = [tuple(x) for x in modified_staking_df.to_numpy()]
     query = '''INSERT INTO  staking_ledger (pk, balance,delegate,epoch_number) 
                 VALUES ( %s, %s, %s, %s) '''
@@ -274,20 +288,23 @@ def main(epoch_no, do_send_email):
         i = 0
         delegate_record_df = pd.DataFrame()
         delegation_record_list = list()
+        blocks_produced_df = get_blocks_produced_for_all(epoch_no)
         for accounts in foundation_accounts_list:
-            delegate_record_df = calculate_payout(delegation_record_list, modified_staking_df, accounts, epoch_no)
+            delegate_record_df = calculate_payout(delegation_record_list, modified_staking_df, accounts, epoch_no,
+                                                  blocks_produced_df)
             i = i + 1
         result = insert_data(delegate_record_df)
-        csv_name=BaseConfig.LOGGING_LOCATION + BaseConfig.CALCULATION_CSV_FILE % (epoch_no)
+        csv_name = BaseConfig.LOGGING_LOCATION + BaseConfig.CALCULATION_CSV_FILE % (epoch_no)
         delegate_record_df.to_csv(csv_name)
         if result == 0:
             insert_into_audit_table(ledger_name, epoch_no)
         logger.info('payouts_calculate complete records for {0}'.format(i))
         # sending emails after payouts calculation completed
         result = epoch_no
-        if do_send_email:
+        if BaseConfig.SEND_EMAIL_TO_BP == 'True':
             send_mail(epoch_no, delegate_record_df)
             # send email to provider with list of 0 block producers
+        if BaseConfig.SEND_SUMMARY_EMAIL == 'True':
             zero_block_producers = delegate_record_df[delegate_record_df['blocks'] == 0]
             zero_block_producers = zero_block_producers[['winner_pub_key']]
             mail_to_foundation_accounts(zero_block_producers, epoch_no)
