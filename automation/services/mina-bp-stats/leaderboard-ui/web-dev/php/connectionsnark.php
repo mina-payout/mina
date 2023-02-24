63eb556b8f351346c7a79489c94b4fbb1aec74bf
umesh.bihani <umesh.bihani@bnt-soft.com>
2023-02-24 14:50:50 +0530

    new UI changes

--- automation/services/mina-bp-stats/leaderboard-ui/web-dev/php/connectionsnark.php
+++ automation/services/mina-bp-stats/leaderboard-ui/web-dev/php/connectionsnark.php
@@ -1,12 +1,10 @@
-
-
 <?php
 
-$username = getenv("DB_SIDECAR_USER");
-$password = getenv("DB_SIDECAR_PWD");
-$database_name = getenv("DB_SIDECAR_DB");
-$port = getenv("DB_SIDECAR_PORT");
-$host = getenv("DB_SIDECAR_HOST");
+$username = getenv("DB_SNARK_USER");
+$password = getenv("DB_SNARK_PWD");
+$database_name = getenv("DB_SNARK_DB");
+$port = getenv("DB_SNARK_PORT");
+$host = getenv("DB_SNARK_HOST");
 $conn = pg_connect("host=".$host." port=".$port." dbname=".$database_name." user=".$username." password=".$password."") or die("Connection failed: " .pg_last_error());
 
 return $conn
--- automation/services/mina-bp-stats/leaderboard-ui/web-dev/php/getPageData.php
+++ automation/services/mina-bp-stats/leaderboard-ui/web-dev/php/getPageData.php
@@ -1,9 +1,6 @@
 <?php
 require_once ("connection.php");
-$myarray = include 'config.php';
 
-$ShowScoreColumn = $myarray[0];
-$MaintenanceMode = $myarray[2];
 
 if (! (isset($_GET['pageNumber']))) {
     $pageNumber = 1;
@@ -13,7 +10,7 @@ if (! (isset($_GET['pageNumber']))) {
 
 $perPageCount = 120;
 
-$sql = "SELECT COUNT(*) FROM nodes WHERE application_status = true and score is not null ";
+$sql = "SELECT COUNT(*) FROM nodes WHERE application_status = true and score is not null";
 
 
 if ($result = pg_query($conn, $sql)) {
@@ -27,79 +24,25 @@ $pagesCount = ceil($rowCount / $perPageCount);
 $lowerLimit = ($pageNumber - 1) * $perPageCount;
 
 
-$sqlQuery = "SELECT block_producer_key , score ,score_percent FROM nodes WHERE application_status = true and score is not null ORDER BY score DESC OFFSET ". ($lowerLimit) . " LIMIT " . ($perPageCount);
+$sqlQuery = "SELECT block_producer_key , score ,score_percent FROM nodes WHERE application_status = true and score is not null ORDER BY score DESC";
 
-$results = pg_query($conn, $sqlQuery);
-$row = pg_fetch_all($results);
+    $results = pg_query($conn, $sqlQuery);
+    $row = pg_fetch_all($results);
+ 
+$maxScoreSidecar= "WITH recentone as (SELECT batch_end_epoch end_epoch , extract('epoch' FROM (to_timestamp(batch_end_epoch) - interval '60' day )) start_epoch
+    FROM bot_logs b WHERE file_timestamps <= CURRENT_TIMESTAMP ORDER BY batch_end_epoch DESC LIMIT 1) 
+    SELECT COUNT(1) ,  to_char(to_timestamp(end_epoch),  'DD-MM-YYYY hh24:mi') as last_modified FROM bot_logs , recentone WHERE batch_start_epoch BETWEEN start_epoch and end_epoch group by 2 ";
+
+        
+    $maxScoreSidecarresult = pg_query($conn, $maxScoreSidecar);
+    $maxScoreRow = pg_fetch_row($maxScoreSidecarresult); 
+    $SidecarmaxScore = $maxScoreRow[0];
+    $Sidecarlast_modified=$maxScoreRow[1];
+    
+      
+    echo json_encode(array('row' => $row, 'rowCount' => $rowCount, 'SidecarmaxScore' => $SidecarmaxScore, 'Sidecarlast_modified'=>$Sidecarlast_modified));
+
+
 
 ?>
-
-<div class="container pr-0 pl-0 mt-0 mb-5" id="results">
-        <div class="table-responsive table-responsive-sm table-responsive-md table-responsive-lg table-responsive-xl">
-            <table class="table table-striped text-center">
-                <thead>
-                    <tr class="border-top-0">
-                        <th scope="col">RANK</th>
-                        <th scope="col" class="text-left">PUBLIC KEY</th>
-                        <?php 
-                        if($ShowScoreColumn == true){
-                        ?>
-                        <th scope="col">SCORE</th>
-                        <?php }?>
-                        <th scope="col">60 Day Uptime Performance SCORE</th>
-                    </tr>
-                </thead>
-                <tbody class="">
-                <tr style="<?php if($MaintenanceMode != true) {echo 'display: none;';}?>">
-                    <td colspan ="<?php if($ShowScoreColumn != true) {echo '3';} else {echo '4';}?>">
-                        <div class="wrap">
-                            <i class="bi bi-exclamation-triangle-fill" style="font-size: 5rem; color: #b0afaf;"></i>
-                            <h1 class="maintenanceText">Under Maintenance</h1>
-                        </div>
-                    </td>
-                </tr>
-                <?php 
-                 $counter = $lowerLimit + 1;
-                 if($MaintenanceMode != true){
-                foreach ($row as $key => $data) { 
-                   
-                    ?>
-                    <tr>
-                        <td scope="row"><?php echo $counter ?></td>
-                        <td><?php echo $data['block_producer_key'] ?></td>
-                        <?php 
-                        if($ShowScoreColumn == true){
-                        ?>
-                        <td><?php echo $data['score'] ?></td>
-                        <?php }?>
-                        <td><?php echo $data['score_percent'] ?> %</td>
-                    </tr>
-                    <?php
-                     $counter++;
-    }
-}
-    ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-
-<div style="height: 30px;"></div>
-
-
-
-<nav aria-label="Page navigation example" style="<?php if($MaintenanceMode == true) {echo 'display: none;';}?>">
-  <ul class="pagination justify-content-center">
-    <li class="<?php if($pageNumber <= 1) {echo 'page-item disabled';} else {echo 'page-item';}?>">
-      <a class="page-link" href="javascript:void(0);" tabindex="-1" onclick="showRecords('<?php echo $perPageCount;  ?>', '<?php  echo 1;  ?>');">First</a>
-    </li>
-    <li class="<?php if($pageNumber <= 1) {echo 'page-item disabled';} else {echo 'page-item';}?>">
-        <a class="page-link" href="javascript:void(0);" onclick="showRecords('<?php echo $perPageCount;  ?>', '<?php if($pageNumber <= 1){ echo $pageNumber; } else { echo ($pageNumber - 1); } ?>');">Prev</a></li>
-    <li class="<?php if($pageNumber == $pagesCount) {echo 'page-item disabled';} else {echo 'page-item';}?>">
-        <a class="page-link" href="javascript:void(0);" onclick="showRecords('<?php echo $perPageCount;  ?>', '<?php if($pageNumber >= $pagesCount){ echo $pageNumber; } else { echo ($pageNumber + 1); } ?>');">Next</a></li>
-    <li class="<?php if($pageNumber == $pagesCount) {echo 'page-item disabled';} else {echo 'page-item';}?>">
-      <a class="page-link" href="javascript:void(0);" onclick="showRecords('<?php echo $perPageCount;  ?>', '<?php  echo $pagesCount;  ?>');">Last</a>
-    </li>
-    <li class = "ml-5 p-2">Page <?php echo $pageNumber; ?> of <?php echo $pagesCount; ?></li>
-  </ul>
-</nav>
+    
